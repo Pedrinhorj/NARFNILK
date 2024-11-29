803f4b90046c8d3c62bf4c39c013df862b6eeb1d
PEDRO ARTHUR SANTOS MACEDO <pedroarthrj@gmail.com>
2024-11-29 19:03:15 -0300

    style: inicia o projeto com html e css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/footer";
+import { FaSearch } from "react-icons/fa";
+import Destaques from "./components/destaques";
+import logo from "./assets/images/Logo-title-option3.png";
+import { LuShoppingCart } from "react-icons/lu";
+import { CgProfile } from "react-icons/cg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="container-header-1">
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <img className="logo" src={logo} alt="" />
+          </a>
+
+          <div className="services">
+            <button>
+              <LuShoppingCart size={50} />
+            </button>
+            <button>
+              <CgProfile size={50} />
+            </button>
+          </div>
+        </div>
+
+        <div className="search">
+          <input
+            type="text"
+            name="search"
+            id=""
+            placeholder="O que você procura?"
+          />
+          <button>
+            <FaSearch size={25} />
+          </button>
+        </div>
+        <nav>
+          <span>Nossa Página de Música</span>
+
+          <span>Nossos Filmes/Séries</span>
+
+          <span>Nossos Livros/HQs</span>
+
+          <span>Nossos Games</span>
+        </nav>
+      </header>
+
+      <main>
+        <section className="container1">
+          <h1>Carrossel</h1>
+        </section>
+
+        <section className="container2">Acordion</section>
+        <section className="container3">
+          <Destaques />
+        </section>
+      </main>
+
+      <Footer />
     </>
   );
 }
