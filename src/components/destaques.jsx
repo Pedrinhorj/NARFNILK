2355ed0b8b13d2030c5c9112064b4e5b040cd5a8
PEDRO ARTHUR SANTOS MACEDO <pedroarthrj@gmail.com>
2024-11-29 20:24:20 -0300

    chore: adiciona um hook do react para esposição de icones

--- src/components/destaques.jsx
+++ src/components/destaques.jsx
@@ -1,14 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import bookone from "../assets/images/Sem Título-1.png";
+import booktwo from "../assets/images/booktwo.png";
+import bookthree from "../assets/images/bookthree.png";
+import "../styles/images.css";
 
 export default function Destaques() {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <div>
       <h1>Nossos principais destaques</h1>
+      <div className="Destaques-main">
+        <article
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <img className="img" src={bookone} alt="" />
+          <h2>Book one</h2>
 
-      <article>
-        <h2>Titulo Livro</h2>
-        <img src="../assets/images/Sem Título-1.png" alt="" />
-      </article>
+          <p>R$39,00</p>
+          <hr />
+
+          {hovered && (
+            <div className="comprar-olhar">
+              <button className="comprar">COMPRAR</button>
+              <button className="olhar">OLHAR</button>
+            </div>
+          )}
+        </article>
+        <article
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <img className="img" src={booktwo} alt="" />
+          <h2>Book Two</h2>
+
+          <p>R$39,00</p>
+          <hr />
+
+          {hovered && (
+            <div className="comprar-olhar">
+              <button className="comprar">COMPRAR</button>
+              <button className="olhar">OLHAR</button>
+            </div>
+          )}
+        </article>
+        <article
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <img className="img" src={bookthree} alt="" />
+          <h2>Book one</h2>
+
+          <p>R$39,00</p>
+          <hr />
+
+          {hovered && (
+            <div className="comprar-olhar">
+              <button className="comprar">COMPRAR</button>
+              <button className="olhar">OLHAR</button>
+            </div>
+          )}
+        </article>
+      </div>
     </div>
   );
 }
